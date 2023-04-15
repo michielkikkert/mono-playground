afd3c7d6056ff00d494e67f94ce389fa1974bb1a
michielkikkert <michiel@kikkert.nl>
2023-04-15 16:09:51 +0200

    Some validation experiments

--- apps/form-directives/src/app/app.component.ts
+++ apps/form-directives/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from './material.module';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 import { MafSelectComponent } from './maf-select/maf-select.component';
 import { CvaDirective } from './directives/cva.directive';
@@ -22,7 +22,7 @@ export class AppComponent {
         { label: 'optie 4', value: 4 },
     ] as Record<string, string | number>[]
     form: FormGroup = new FormGroup<any>({
-        test: new FormControl('test'),
+        test: new FormControl('test', [Validators.email, Validators.required]),
         test2: new FormControl(this.selectItems[0]),
         select: new FormControl(this.selectItems[1]),
         // textarea: new FormControl('I\'m a text area')
--- apps/form-directives/src/app/maf-input/maf-input.component.ts
+++ apps/form-directives/src/app/maf-input/maf-input.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgControl } from '@angular/forms';
+import { AbstractControl, FormsModule, NgControl } from '@angular/forms';
 import { MaterialModule } from '../material.module';
 import { MatInput } from '@angular/material/input';
 
@@ -9,23 +9,38 @@ import { MatInput } from '@angular/material/input';
     standalone: true,
     imports: [CommonModule, FormsModule, MaterialModule],
     template: `
-      <mat-form-field appearance="fill">
-        <mat-label>{{label}} ({{type}})</mat-label>
-        <input matInput [(ngModel)]="value" [disabled]="disabled"/>
-      </mat-form-field>`,
+        <mat-form-field appearance="fill">
+            <mat-label>{{ label }} ({{ type }})</mat-label>
+            <input matInput [(ngModel)]="value" [disabled]="disabled" />
+        </mat-form-field>
+        <mat-error *ngIf="control.hasError('email') && !control.hasError('required')"> Please enter a valid email address </mat-error>
+        <mat-error *ngIf="control.hasError('email')">
+            {{ control.errors | json }}
+        </mat-error>
+    `,
+    styles: [':host.ng-invalid .mat-mdc-text-field-wrapper { border: 1px solid red;  }'],
 })
 export class MafInputComponent implements OnInit {
-    @ViewChild(MatInput, { static: true }) input!: MatInput;
-	@Input() disabled: boolean = false;
+    @ViewChild(MatInput, { static: true }) input!: MatInput | null;
+    @Input() disabled = false;
     @Input() type!: string;
     @Input() label!: string;
     @Input() items: Record<string, string | number>[] = [];
     control = inject(NgControl);
 
-    get value() {return this.control.value;}
-	set value(v: string) {this.control?.control?.setValue(v);}
+    get value() {
+        return this.control.value;
+    }
+    set value(v: string) {
+        this.control?.control?.setValue(v);
+    }
 
     ngOnInit() {
-        this.input.disabled = true
+
+
+        this.input?.ngControl?.control?.setErrors([{custom: 'error'}]);
+        // this.input.disabled = true
+        console.log('HAS ERROR', this.control.hasError('email'));
+        // this.control?.statusChanges?.subscribe( status => console.log(status) )
     }
 }
